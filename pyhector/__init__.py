0d97e6ed6c99ff961963fc4f4a725fd05873ba0a
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-01-26 17:28:07 +0100

    Read emissions source list from module file

--- pyhector/__init__.py
+++ pyhector/__init__.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from .default_config import default_config
 from .units import units
+from .emissions import emissions
 
 _lib = np.ctypeslib.load_library('libpyhector', pkg_resources.resource_filename(__name__, '..'))
 _lib.open.restype = ctypes.c_int
@@ -92,9 +93,21 @@ class PyHector():
 
 
     def set_emissions(self, scenario):
-        for section, variable, years, values, unit in _convert_emissions(scenario):
-            self._check(
-                _lib.set_array(self.__state, _conv(section), _conv(variable), years.astype(np.int32), values.astype(np.float64), len(years)))
+        for section in emissions:
+            for source in emissions[section]:
+                if source not in scenario.columns:
+                    continue
+                years = np.array(scenario.index, dtype=np.int32)
+                values = np.array(scenario[source], dtype=np.float64)
+                self._check(
+                    _lib.set_array(
+                        self.__state,
+                        _conv(section),
+                        _conv(source),
+                        years,
+                        values,
+                        len(years))
+                    )
 
 
 def read_hector_input(csv_file):
@@ -113,64 +126,6 @@ rcp60 = read_hector_input(os.path.join(os.path.dirname(__file__),
     './emissions/RCP6_emissions.csv'))
 rcp85 = read_hector_input(os.path.join(os.path.dirname(__file__),
     './emissions/RCP85_emissions.csv'))
-
-
-# TODO Revise
-def _convert_emissions(scenario):
-    emissions = {
-        "simpleNbox": ["anthroEmissions", "lucEmissions"],
-        "so2": ["SO2_emissions"],
-        "CH4": ["CH4_emissions"],
-        "OH": ["NOX_emissions", "CO_emissions", "NMVOC_emissions"],
-        "ozone": ["NOX_emissions", "CO_emissions", "NMVOC_emissions"],
-        "N2O": ["N2O_emissions"],
-        "bc": ["BC_emissions"],
-        "oc": ["OC_emissions"],
-        "CF4_halocarbon": ["CF4_emissions"],
-        "C2F6_halocarbon": ["C2F6_emissions"],
-        "C4F10_halocarbon": ["C4F10_emissions"],  # commented out in RCP
-        "HFC23_halocarbon": ["HFC23_emissions"],
-        "HFC32_halocarbon": ["HFC32_emissions"],
-        "HFC4310_halocarbon": ["HFC4310_emissions"],
-        "HFC125_halocarbon": ["HFC125_emissions"],
-        "HFC134a_halocarbon": ["HFC134a_emissions"],
-        "HFC143a_halocarbon": ["HFC143a_emissions"],
-        "HFC152a_halocarbon": ["HFC152a_emissions"],  # commented out in RCP
-        "HFC227ea_halocarbon": ["HFC227ea_emissions"],
-        "HFC245fa_halocarbon": ["HFC245fa_emissions"],
-        "HFC236fa_halocarbon": ["HFC236fa_emissions"],  # commented out in RCP
-        "SF6_halocarbon": ["SF6_emissions"],
-        "CFC11_halocarbon": ["CFC11_emissions"],
-        "CFC12_halocarbon": ["CFC12_emissions"],
-        "CFC113_halocarbon": ["CFC113_emissions"],
-        "CFC114_halocarbon": ["CFC114_emissions"],
-        "CFC115_halocarbon": ["CFC115_emissions"],
-        "CCl4_halocarbon": ["CCl4_emissions"],
-        "CH3CCl3_halocarbon": ["CH3CCl3_emissions"],
-        "halon1211_halocarbon": ["halon1211_emissions"],
-        "halon1301_halocarbon": ["halon1301_emissions"],
-        "halon2402_halocarbon": ["halon2402_emissions"],
-        "HCF22_halocarbon": ["HCF22_emissions"],
-        "HCF141b_halocarbon": ["HCF141b_emissions"],
-        "HCF142b_halocarbon": ["HCF142b_emissions"],
-        "HCF143_halocarbon": ["HCF143_emissions"],  # commented out in RCP
-        "CH3Cl_halocarbon": ["CH3Cl_emissions"],
-        "CH3Br_halocarbon": ["CH3Br_emissions"]
-    }
-    # List of tuples [("so2", "SO2_emissions", [... array ], unit), ...]
-    output = []
-    for category in emissions:
-        for source in emissions[category]:
-            if source in scenario:
-                gas = scenario[source]
-                output.append((
-                  category,
-                  source,
-                  np.array(gas.index),
-                  np.array(gas),
-                  units[source],
-                ))
-    return output
 
 
 def run(scenario, config_options=None):
---
+++ pyhector/emissions.py
@@ -0,0 +1,42 @@
+# Emissions mapping for configuration.
+
+emissions = {
+    "simpleNbox": ["anthroEmissions", "lucEmissions"],
+    "so2": ["SO2_emissions"],
+    "CH4": ["CH4_emissions"],
+    "OH": ["NOX_emissions", "CO_emissions", "NMVOC_emissions"],
+    "ozone": ["NOX_emissions", "CO_emissions", "NMVOC_emissions"],
+    "N2O": ["N2O_emissions"],
+    "bc": ["BC_emissions"],
+    "oc": ["OC_emissions"],
+    "CF4_halocarbon": ["CF4_emissions"],
+    "C2F6_halocarbon": ["C2F6_emissions"],
+    "C4F10_halocarbon": ["C4F10_emissions"],  # commented out in RCP
+    "HFC23_halocarbon": ["HFC23_emissions"],
+    "HFC32_halocarbon": ["HFC32_emissions"],
+    "HFC4310_halocarbon": ["HFC4310_emissions"],
+    "HFC125_halocarbon": ["HFC125_emissions"],
+    "HFC134a_halocarbon": ["HFC134a_emissions"],
+    "HFC143a_halocarbon": ["HFC143a_emissions"],
+    "HFC152a_halocarbon": ["HFC152a_emissions"],  # commented out in RCP
+    "HFC227ea_halocarbon": ["HFC227ea_emissions"],
+    "HFC245fa_halocarbon": ["HFC245fa_emissions"],
+    "HFC236fa_halocarbon": ["HFC236fa_emissions"],  # commented out in RCP
+    "SF6_halocarbon": ["SF6_emissions"],
+    "CFC11_halocarbon": ["CFC11_emissions"],
+    "CFC12_halocarbon": ["CFC12_emissions"],
+    "CFC113_halocarbon": ["CFC113_emissions"],
+    "CFC114_halocarbon": ["CFC114_emissions"],
+    "CFC115_halocarbon": ["CFC115_emissions"],
+    "CCl4_halocarbon": ["CCl4_emissions"],
+    "CH3CCl3_halocarbon": ["CH3CCl3_emissions"],
+    "halon1211_halocarbon": ["halon1211_emissions"],
+    "halon1301_halocarbon": ["halon1301_emissions"],
+    "halon2402_halocarbon": ["halon2402_emissions"],
+    "HCF22_halocarbon": ["HCF22_emissions"],
+    "HCF141b_halocarbon": ["HCF141b_emissions"],
+    "HCF142b_halocarbon": ["HCF142b_emissions"],
+    "HCF143_halocarbon": ["HCF143_emissions"],  # commented out in RCP
+    "CH3Cl_halocarbon": ["CH3Cl_emissions"],
+    "CH3Br_halocarbon": ["CH3Br_emissions"]
+}
