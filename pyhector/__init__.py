7260348e6d78f174f7fc4f57f0869a79a0600573
Sven Willner <sven.willner@gmail.com>
2017-01-30 15:06:35 +0100

    Update __init__.py

--- pyhector/__init__.py
+++ pyhector/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2017 pyhector authors
 # Sven Willner <sven.willner@pik-potsdam.de>
 # Robert Gieseke <robert.gieseke@pik-potsdam.de>
-# Free software under GNU Affero General Public License v3, see LICENSE.md
+# Free software under GNU Affero General Public License v3, see LICENSE
 
 # -*- coding: utf-8 -*-
 
