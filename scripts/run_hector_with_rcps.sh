cadc545405749eec5dc7d0a2c2f539f1354efdd3
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-02-22 16:20:56 +0100

    Fix script to run RCPs and update data

--- scripts/run_hector_with_rcps.sh
+++ scripts/run_hector_with_rcps.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-cd ../src/hector
+# This script runs `hector` in the `tests/hector` directory and copies
+# the output files for comparison with `pyhector`.
+
+scriptdir=$(dirname -- "$(readlink -e -- "$BASH_SOURCE")")
+cd $scriptdir
+cd ../tests/hector
 
 for RF in 26 45 60 85; do
   ./source/hector input/hector_rcp$RF.ini
-  cp output/outputstream_rcp$RF.csv ../../tests/data
+  cp output/outputstream_rcp$RF.csv ../data
 done
