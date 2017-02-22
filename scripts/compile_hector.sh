ab2eba9642e1852f994fd4e83d441a86827600dc
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-02-22 16:16:54 +0100

    Let compilation script run independent of call location
    Compiles the version of hector in `tests` directory.
    [skip ci]

--- scripts/compile_hector.sh
+++ scripts/compile_hector.sh
@@ -4,10 +4,13 @@
 # See also https://github.com/JGCRI/hector/wiki/BuildHector
 
 ver=`awk '/define.*BOOST_LIB_VERSION/ {print $3}' /usr/include/boost/version.hpp`
+echo Compiling standalone Hector in "tests/hector" directory ...
 echo Boost version $ver
 
-echo Compiling standalone Hector...
-cd ../src/hector
+scriptdir=$(dirname -- "$(readlink -e -- "$BASH_SOURCE")")
+
+cd $scriptdir
+cd ../tests/hector
 BOOSTLIB=/usr/local/lib   BOOSTVERSION=$ver   BOOSTROOT=/usr/include/boost \
   make hector $@
 
