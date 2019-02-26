fd9979b1023ed9ed2d8d4720281da3726948a2fd
Sven Willner <sven.willner@gmail.com>
2019-02-26 14:25:42 +0100

    Adjust setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ import versioneer
 
 path = os.path.abspath(os.path.dirname(__file__))
 
+
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
@@ -55,7 +56,12 @@ cmdclass.update({"test": PyTest})
 libpyhector = Extension(
     "pyhector._binding",
     language="c++",
-    include_dirs=["include", "hector/headers", get_pybind_include(), get_pybind_include(user=True)],
+    include_dirs=[
+        "include",
+        "hector/headers",
+        get_pybind_include(),
+        get_pybind_include(user=True),
+    ],
     libraries=["m", "boost_system", "boost_filesystem"],
     extra_compile_args=["-std=c++11"],
     sources=list(
@@ -68,35 +74,34 @@ libpyhector = Extension(
     depends=list(glob.glob("include/*.h") + glob.glob("hector/headers/**/*.hpp")),
 )
 
-with open(os.path.join(path, 'README.rst'), "r") as f:
+with open(os.path.join(path, "README.rst"), "r") as f:
     readme = f.read()
 
 setup(
     name="pyhector",
     version=versioneer.get_version(),
     cmdclass=cmdclass,
-    description='Python wrapper for the Hector simple climate model',
+    description="Python wrapper for the Hector simple climate model",
     long_description=readme,
     long_description_content_type="text/x-rst",
-    url='https://github.com/openclimatedata/pyhector',
-    author='Sven Willner, Robert Gieseke',
-    author_email='sven.willner@pik-potsdam.de, robert.gieseke@pik-potsdam.de',
-    license='GNU Affero General Public License v3',
-    platforms='any',
+    url="https://github.com/openclimatedata/pyhector",
+    author="Sven Willner, Robert Gieseke",
+    author_email="sven.willner@pik-potsdam.de, robert.gieseke@pik-potsdam.de",
+    license="GNU Affero General Public License v3",
+    platforms="any",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='climate model climate change',
-    package_data={'pyhector': ['rcp_default.ini', 'emissions/*']},
+    keywords="climate model climate change",
+    package_data={"pyhector": ["rcp_default.ini", "emissions/*"]},
     include_package_data=True,
     packages=["pyhector"],
-    install_requires=["pandas", "numpy", "pybind11>=2.2"],
-    tests_require=["pytest"],
-    setup_requires=["pytest-runner"],
+    install_requires=["numpy", "pandas", "pybind11>=2.2"],
+    tests_require=["pytest>=4.0", "pytest-cov"],
     ext_modules=[libpyhector],
     zip_safe=False,
 )
