bcad8751f690a95eba8f86fa963037b65f3e332a
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2019-03-25 18:42:02 +0100

    Update to Hector 2.2.0

--- scripts/write_constants_py.sh
+++ scripts/write_constants_py.sh
@@ -6,8 +6,8 @@
 #   ./scripts/write_constants_py.sh > pyhector/constants.py
 
 HECTOR_ROOT=./hector
-HECTOR_SOURCE=$HECTOR_ROOT/source
-HECTOR_HEADERS=$HECTOR_ROOT/headers
+HECTOR_SOURCE=$HECTOR_ROOT/src
+HECTOR_HEADERS=$HECTOR_ROOT/inst/include
 
 echo '"""'
 echo 'Internal mapping of Hector components and variables.'
@@ -15,15 +15,15 @@ echo 'Auto-generated from Hector headers.'
 echo -e '"""\n'
 
 echo 'components = {'; \
-(echo '#include "components/component_names.hpp"'; \
+(echo '#include "hector/inst/include/component_names.hpp"'; \
   sed -n -e 's/^#define *\([^[:space:]]*\).*$/v\1=\1;/p' \
-      $HECTOR_HEADERS/components/component_names.hpp) \
+      $HECTOR_HEADERS/component_names.hpp) \
 		| gcc -E -I $HECTOR_HEADERS - \
 		| sed -n -e 's/" "//g;s/^v\(.*\)_COMPONENT_NAME="\(.*\)";$/    "\1": "\2",/p'; \
 		echo '}'; #\
 		echo -e '\nvariables = {'; \
-		(echo '#include "components/component_data.hpp"'; \
-			sed -n -e 's/^#define  *\([^[:space:]]*\).*$/v\1=\1;/p' $HECTOR_HEADERS/components/component_data.hpp) \
+		(echo '#include "hector/inst/include/component_data.hpp"'; \
+			sed -n -e 's/^#define  *\([^[:space:]]*\).*$/v\1=\1;/p' $HECTOR_HEADERS/component_data.hpp) \
 		| gcc -E -I $HECTOR_HEADERS - \
 		| sed -n -e 's/" "//g;s/^vD_\(.*\)="\(.*\)";$/    "\1": "\2",/p'; \
 		echo '}'
