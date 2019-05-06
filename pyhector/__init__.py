1f20d44a664d2584f479a163d44105ace7eac312
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2019-05-06 18:30:10 +0200

    Re-enable Hector version
    Closes #48

--- pyhector/__init__.py
+++ pyhector/__init__.py
@@ -24,6 +24,7 @@ import numpy as np
 import pandas as pd
 
 from ._binding import _Hector  # pylint: disable=no-name-in-module
+from ._binding import __hector_version__  # pylint: disable=no-name-in-module
 from ._version import get_versions
 from .default_config import _default_config
 from .emissions import emissions
