60418a03bfb3c11a91d8ebdbe857871a416e672c
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2019-05-22 14:41:05 +0200

    Update run script to new Hector paths

--- scripts/run_hector_with_rcps.sh
+++ scripts/run_hector_with_rcps.sh
@@ -4,9 +4,9 @@
 # the output files for comparison with `pyhector`.
 
 scriptdir=$(dirname -- "$(readlink -e -- "$BASH_SOURCE")")
-cd $scriptdir/../hector
+cd $scriptdir/../hector/inst
 
 for RF in 26 45 60 85; do
-  ./source/hector input/hector_rcp$RF.ini
+  ../src/hector input/hector_rcp$RF.ini
   cp output/outputstream_rcp$RF.csv $scriptdir/../tests/data
 done
