f91d4abee65732fb3f7193df6cae1bcd206ce3b6
Robert Gieseke <rob.g@web.de>
2023-06-17 18:21:15 +0200

    Update testing APIs from Pandas

--- tests/test_pyhector.py
+++ tests/test_pyhector.py
@@ -4,7 +4,7 @@ import os
 from copy import deepcopy
 
 import pandas as pd
-from pandas.util.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 import pyhector
 from pyhector import (
@@ -59,7 +59,7 @@ def test_rcps():
             output["temperature.Tgav"],
             original.Tgav,
             check_names=False,
-            check_less_precise=True,
+            check_exact=False,
         )
 
 
